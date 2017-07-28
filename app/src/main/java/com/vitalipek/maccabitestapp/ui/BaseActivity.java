968e0c3dd8b3608c58b24813e8c6c625fff9513b
pedalka <pedalka@gmail.com>
2017-07-28 23:51:49 +0300

    mission ID:3 setup base activity

---
+++ app/src/main/java/com/vitalipek/maccabitestapp/ui/BaseActivity.java
@@ -0,0 +1,52 @@
+package com.vitalipek.maccabitestapp.ui;
+
+import android.support.annotation.LayoutRes;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewStub;
+
+import com.vitalipek.maccabitestapp.R;
+
+/**
+ * Created by Vitali on 28/07/2017.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity
+{
+    private View progressBar;
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID)
+    {
+        View baseLayout = getLayoutInflater().inflate(R.layout.activity_base, null, false);
+
+        ViewStub actContent = baseLayout.findViewById(R.id.base_vs_activity_content);
+        actContent.setLayoutResource(layoutResID);
+        actContent.inflate();
+
+        super.setContentView(baseLayout);
+        initUi();
+    }
+
+    private void initUi()
+    {
+        progressBar = findViewById(R.id.base_pb_container);
+    }
+
+    public void startProgress()
+    {
+        if(progressBar!=null)
+        {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void stopProgress()
+    {
+        if(progressBar!=null)
+        {
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+}
